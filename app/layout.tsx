926512ff33022c92e4faee202083a1ea3d7f5b74
Nikita <poker6-96@mail.com>
2023-06-08 14:32:56 +0300

    Register & Login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import ToasterContext from "@/app/context/ToasterContext";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'React Messenger',
-  description: 'React messenger project',
+    title: 'React Messenger',
+    description: 'React messenger project',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <ToasterContext/>
+        {children}
+        </body>
+        </html>
+    )
 }
